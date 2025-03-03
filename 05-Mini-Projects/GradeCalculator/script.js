2022b047cf2a5f279da45666ebe7da364c954ec4
Supachai-Sungsirn <supachai.sun@spumail.net>
2025-03-03 17:49:05 +0700

    เกรด

--- 05-Mini-Projects/GradeCalculator/script.js
+++ 05-Mini-Projects/GradeCalculator/script.js
@@ -14,10 +14,13 @@ function calculateGPA() {
 
     // ฟังก์ชันแปลงคะแนน 0-100 เป็นคะแนนเต็ม 4.00
     function convertToGPA(score) {
-        if (score >= 90) return 4.00;
-        else if (score >= 80) return 3.00;
-        else if (score >= 70) return 2.00;
-        else if (score >= 60) return 1.00;
+        if (score >= 80) return 4.00;
+        else if (score >= 75) return 3.50;
+        else if (score >= 70) return 3.00;
+        else if (score >= 65) return 2.50;
+        else if (score >= 60) return 2.00;
+        else if (score >= 55) return 1.50;
+        else if (score >= 50) return 1.00;
         else return 0.00;
     }
 
